d2b13351d7e8ab9a453a0a6555ff966009d00517
Sam Choukri <sam@choukri.net>
2018-12-04 00:51:42 -0800

    cleanup perl version a little

--- 04/run.pl
+++ 04/run.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-sub top_item_by_value($);
+sub top_key_by_value($);
 
 my @lines = <>;
 chomp(@lines);
@@ -43,9 +43,9 @@ for (my $i = 0; $i < scalar @lines;) {
   }
 }
 
-my($winner_id) = top_item_by_value(\%asleep);
+my $winner_id = top_key_by_value(\%asleep);
 
-my($top_min) = top_item_by_value($hist{$winner_id});
+my $top_min = top_key_by_value($hist{$winner_id});
 
 my $part1 = $winner_id * $top_min;
 print "part 1: $part1\n";
@@ -53,18 +53,18 @@ print "part 1: $part1\n";
 my %top_min_per_guard;
 my %most_min_per_guard;
 while (my($id, $h) = each %hist) {
-  my($min, $count) = top_item_by_value($h);
+  my $min = top_key_by_value($h);
   $top_min_per_guard{$id} = $min;
-  $most_min_per_guard{$id} = $count;
+  $most_min_per_guard{$id} = $h->{$min};
 }
 
-my($winner2_id) = top_item_by_value(\%most_min_per_guard);
+my $winner2_id = top_key_by_value(\%most_min_per_guard);
 
 my $part2 = $winner2_id * $top_min_per_guard{$winner2_id};
 print "part 2: $part2\n";
 
-sub top_item_by_value($) {
+sub top_key_by_value($) {
   my $href = $_[0] || die "hashref not specified";
   my($key) = sort {$href->{$b} <=> $href->{$a}} keys %$href;
-  return ($key, $href->{$key});
+  return $key;
 }
