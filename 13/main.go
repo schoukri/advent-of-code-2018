c4493fa6a035e5e8006b3875a0af8787e3f5da6f
Sam Choukri <sam@choukri.net>
2019-03-10 22:23:43 -0700

    finally got around to finishing day 13, part 2

--- 13/main.go
+++ 13/main.go
@@ -45,6 +45,7 @@ type Cart struct {
 	X, Y      int
 	Direction Direction
 	Turn      Turn
+	Crashed   bool
 }
 
 type Pieces map[int]map[int]Piece
@@ -69,9 +70,6 @@ func main() {
 
 	track := NewTrack(lines)
 
-	// jsonBytes, _ := json.MarshalIndent(track, "", "  ")
-	// fmt.Printf("Track: %s\n", string(jsonBytes))
-
 	printCart := false
 	if strings.Contains(*filePath, "sample") {
 		printCart = true
@@ -80,7 +78,6 @@ func main() {
 TICK:
 	for tick := 1; ; tick++ {
 
-		fmt.Printf("TICK: %d\n", tick)
 		sort.Sort(ByPosition(track.Carts))
 		track.Print(printCart)
 
@@ -95,21 +92,17 @@ TICK:
 		}
 
 		if *part == 2 {
-			num := track.RemoveCrashedCars()
-			fmt.Printf("RemoveCrashedCars: %d (tick=%d)\n", num, tick)
-
-			if len(track.Carts) < 2 {
+			numRemaining := track.RemoveCrashedCarts()
+			if numRemaining == 0 {
+				log.Fatal("no carts left on the track")
+			} else if numRemaining == 1 {
 				cart := track.Carts[0]
 				fmt.Printf("part 2: %d,%d (tick=%d)\n", cart.X, cart.Y, tick)
-				fmt.Printf("cart: %#v\n", cart)
-				track.Print(true)
 				break TICK
 			}
 		}
 
 	}
-	track.Print(printCart)
-
 }
 
 func (t *Track) Print(show bool) {
@@ -146,90 +139,48 @@ func (t *Track) Print(show bool) {
 	}
 }
 
-func (t *Track) RemoveCrashedCars() int {
-
-	sort.Sort(ByPosition(t.Carts))
-
-	count := make(map[int]map[int]int)
-	for _, cart := range t.Carts {
-		if _, ok := count[cart.X]; !ok {
-			count[cart.X] = make(map[int]int)
-		}
-		count[cart.X][cart.Y]++
-	}
-
-	fmt.Println("--------------------------")
+func (t *Track) RemoveCrashedCarts() int {
 	remaining := make([]*Cart, 0)
-	removed := 0
 	for _, cart := range t.Carts {
-		if count[cart.X][cart.Y] == 1 {
-			remaining = append(remaining, cart)
-		} else {
-			fmt.Printf("REMOVED DUPE CART: %#v\n", cart)
-			removed++
+		if cart.Crashed {
+			continue
 		}
+		remaining = append(remaining, cart)
 	}
-
 	t.Carts = remaining
-
-	return removed
+	return len(t.Carts)
 }
 
 func (t *Track) Move(c *Cart) bool {
-
-	//fmt.Printf("Move  Start: %#v\n", c)
 
 	piece, ok := t.Pieces[c.X][c.Y]
 	if !ok {
 		log.Fatalf("missing track piece at %d,%d\n", c.X, c.Y)
 	}
 
-	newTurn := c.Turn
 	newDirection := c.Direction
 
 	switch piece {
-	case Vertical:
-		switch c.Direction {
-		case North:
-			c.Y--
-		case South:
-			c.Y++
-		}
-	case Horizontal:
-		switch c.Direction {
-		case West:
-			c.X--
-		case East:
-			c.X++
-		}
 	case CurveBack:
 		switch c.Direction {
 		case North:
-			c.X--
 			newDirection = West
 		case South:
-			c.X++
 			newDirection = East
 		case West:
-			c.Y--
 			newDirection = North
 		case East:
-			c.Y++
 			newDirection = South
 		}
 	case CurveForward:
 		switch c.Direction {
 		case North:
-			c.X++
 			newDirection = East
 		case South:
-			c.X--
 			newDirection = West
 		case West:
-			c.Y++
 			newDirection = South
 		case East:
-			c.Y--
 			newDirection = North
 		}
 	case Intersection:
@@ -237,6 +188,7 @@ func (t *Track) Move(c *Cart) bool {
 		// it turns left the first time, goes straight the second time,
 		// turns right the third time, and then repeats those directions
 
+		var newTurn Turn
 		switch c.Turn {
 		case Right:
 			newTurn = Left
@@ -245,64 +197,59 @@ func (t *Track) Move(c *Cart) bool {
 		case Straight:
 			newTurn = Right
 		}
+		c.Turn = newTurn
 
 		switch c.Direction {
 		case North:
 			switch c.Turn {
 			case Right:
 				newDirection = East
-				c.X++
 			case Left:
 				newDirection = West
-				c.X--
-			case Straight:
-				c.Y--
 			}
 		case South:
 			switch c.Turn {
 			case Right:
 				newDirection = West
-				c.X--
 			case Left:
 				newDirection = East
-				c.X++
-			case Straight:
-				c.Y++
 			}
 		case East:
 			switch c.Turn {
 			case Right:
 				newDirection = South
-				c.Y++
 			case Left:
 				newDirection = North
-				c.Y--
-			case Straight:
-				c.X++
 			}
 		case West:
 			switch c.Turn {
 			case Right:
 				newDirection = North
-				c.Y--
 			case Left:
 				newDirection = South
-				c.Y++
-			case Straight:
-				c.X--
 			}
 		}
 	}
 
-	c.Turn = newTurn
 	c.Direction = newDirection
 
-	//fmt.Printf("Move Finish: %#v\n", c)
+	switch c.Direction {
+	case North:
+		c.Y--
+	case South:
+		c.Y++
+	case West:
+		c.X--
+	case East:
+		c.X++
+	}
 
 	// does another cart exist at the same location?
 	// If so, it's a crash!
 	for _, cart := range t.Carts {
-		if cart.X == c.X && cart.Y == c.Y && cart.ID != c.ID {
+		if cart.X == c.X && cart.Y == c.Y && cart.ID != c.ID && !cart.Crashed {
+			cart.Crashed = true
+			c.Crashed = true
 			return false
 		}
 	}
