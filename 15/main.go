0301aa2173d5d308c95da7ba32a3723b01969e18
Sam Choukri <sam@choukri.net>
2019-05-02 02:05:08 -0700

    day 15 part 1: rewrite as a graph and use BFS to calculate shortest path (much faster!)

--- 15/main.go
+++ 15/main.go
@@ -6,10 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"sort"
-	"strings"
 )
 
 type Type rune
@@ -22,15 +20,22 @@ const (
 )
 
 type Piece struct {
-	X, Y      int
 	Type      Type
 	HitPoints int
 }
 
-type Grid [][]*Piece
+type Node struct {
+	ID    int
+	X, Y  int
+	Piece *Piece
+}
 
-type Path []*Piece
-type Paths []Path
+type Graph struct {
+	Nodes []*Node
+	Edges [][]bool
+}
+
+type Path []*Node
 
 func main() {
 
@@ -43,52 +48,45 @@ func main() {
 		log.Fatalf("cannot read file %s: %v", *filePath, err)
 	}
 
-	grid := NewGrid(lines)
+	graph := NewGraph(lines)
 
 ROUND:
 	for round := 1; ; round++ {
 
 		fmt.Printf("ROUND: %d\n", round)
-		//grid.Print()
 
 		// get a list of all units, in the order they will take a turn
-		units := make([]*Piece, 0)
-		for _, row := range grid {
-			for _, piece := range row {
-				if piece.Type == TypeElf || piece.Type == TypeGoblin {
-					units = append(units, piece)
-				}
+		units := make([]*Node, 0)
+		for _, node := range graph.Nodes {
+			if node.Piece != nil {
+				units = append(units, node)
 			}
 		}
+		sort.Sort(ByPosition(units))
 
 		for _, unit := range units {
 
-			// skip units that have died (become an empty square)
-			if !(unit.Type == TypeElf || unit.Type == TypeGoblin) {
+			// skip any units that are dead
+			if unit.Piece == nil {
 				continue
 			}
 
 			// if the unit is already in range of another unit, attack it (and don't move)
-			if grid.AttackInRangeTarget(unit) {
+			if graph.AttackInRangeTarget(unit) {
 				continue
 			}
 
-			targets := grid.Targets(unit)
+			targets := graph.Targets(unit)
 			if len(targets) == 0 {
-				fmt.Printf("part 1: %d\n", grid.TotalHitPoints()*(round-1))
+				fmt.Printf("part 1: %d\n", graph.TotalHitPoints()*(round-1))
 				break ROUND
 			}
 
-			minPath := math.MaxInt32
-			allPaths := make(Paths, 0)
+			allPaths := make([]Path, 0)
 			for _, target := range targets {
-				for _, open := range grid.OpenSquares(target) {
-					paths := grid.GetPaths(unit, open, minPath)
-					if len(paths) > 0 {
-						sort.Sort(ByShortestPath(paths))
-						if len(paths[0]) < minPath {
-							minPath = len(paths[0])
-						}
+				for _, adj := range graph.Adjacent(target) {
+					if adj.Piece == nil { // is open?
+						paths := graph.GetPaths(unit, adj)
 						allPaths = append(allPaths, paths...)
 					}
 				}
@@ -102,130 +100,121 @@ ROUND:
 			sort.Sort(ByShortestPath(allPaths))
 			path := allPaths[0]
 
-			// move to the unit to the first square in the path
-			grid.Move(unit, path[0])
+			// move the unit to the first adjacent square in the path
+			dest := path[1]
+			graph.Move(unit, dest)
 
 			// launch attack if the move put this piece in range of another
-			grid.AttackInRangeTarget(unit)
+			graph.AttackInRangeTarget(dest)
 		}
 	}
-
 }
 
-func (grid Grid) GetPaths(start, end *Piece, minPath int) Paths {
-	path := make(Path, 0)
-	paths := make([]Path, 0)
-	//minPath := math.MaxInt32
-	return grid.getPaths(start, end, minPath, path, paths)
-}
+func (g *Graph) GetPaths(start, end *Node) []Path {
+	paths := make([][]int, 0)
 
-func (grid Grid) getPaths(start, end *Piece, minPath int, path Path, paths []Path) []Path {
-	seen := make(map[Piece]bool)
-	for _, piece := range path {
-		seen[*piece] = true
+	queue := make([]int, 0)
+	visited := make([]bool, len(g.Nodes))
+	pred := make(map[int]int)
+
+	queue = append(queue, start.ID)
+	visited[start.ID] = true
+	pred[start.ID] = -1
+
+QUEUE:
+	for len(queue) > 0 {
+		// pop
+		currID := queue[0]
+		queue = queue[1:]
+
+	EDGE:
+		for nodeID, ok := range g.Edges[currID] {
+			if !ok || visited[nodeID] {
+				continue
+			}
+			if nodeID == end.ID {
+				path := make([]int, 0)
+				path = append(path, nodeID)
+				for p := currID; p > -1; {
+					path = append(path, p)
+					if pNext, ok := pred[p]; ok {
+						p = pNext
+					}
+				}
+				if len(paths) > 0 {
+					// compare the length of this path to the last path added to the list
+					if len(path) > len(paths[len(paths)-1]) {
+						break QUEUE
+					}
+				}
+				paths = append(paths, path)
+				break EDGE
+			}
+			// add node to queue if its open
+			if g.Nodes[nodeID].Piece == nil {
+				queue = append(queue, nodeID)
+			}
+			visited[nodeID] = true
+			pred[nodeID] = currID
+		}
 	}
-	nextRound := make([]*Piece, 0)
-	for _, open := range grid.OpenSquares(start) {
-		if _, ok := seen[*open]; ok {
+	realPaths := make([]Path, len(paths))
+	for i, path := range paths {
+		for j := len(path) - 1; j >= 0; j-- {
+			realPaths[i] = append(realPaths[i], g.Nodes[path[j]])
+		}
+
+	}
+	//fmt.Printf("paths (%d -> %d): %+v\n", start.ID, end.ID, paths)
+	return realPaths
+}
+
+func (g *Graph) Move(source, dest *Node) {
+	if source.Piece == nil {
+		log.Fatalf("source piece is empty: %+v", source)
+	}
+	if dest.Piece != nil {
+		log.Fatalf("dest piece is not empty: %+v", dest)
+	}
+
+	// swap pieces
+	source.Piece, dest.Piece = dest.Piece, source.Piece
+}
+
+func (g *Graph) Targets(node *Node) []*Node {
+	if node.Piece == nil {
+		log.Fatalf("node does not have a piece: %+v", node)
+	}
+	targets := make([]*Node, 0)
+	for _, target := range g.Nodes {
+		if target.Piece == nil || node.Piece.Type == target.Piece.Type {
 			continue
 		}
-		if open.X == end.X && open.Y == end.Y {
-			path = append(path, open)
-			if len(path) < minPath {
-				minPath = len(path)
-			}
-			paths = append(paths, path)
-			return paths
-		}
-		if len(path) <= minPath {
-			nextRound = append(nextRound, open)
-		}
+		targets = append(targets, target)
 	}
-
-	for _, open := range nextRound {
-		pathCopy := make(Path, len(path))
-		copy(pathCopy, path)
-		pathCopy = append(pathCopy, open)
-		paths = grid.getPaths(open, end, minPath, pathCopy, paths)
-	}
-
-	return paths
+	return targets
 }
 
-func (grid Grid) getPathsOne(start, end *Piece, path Path, paths []Path) []Path {
-	seen := make(map[Piece]bool)
-	for _, piece := range path {
-		seen[*piece] = true
+func (g *Graph) Adjacent(node *Node) []*Node {
+	nodes := make([]*Node, 0)
+	for adjID, ok := range g.Edges[node.ID] {
+		if ok {
+			nodes = append(nodes, g.Nodes[adjID])
+		}
 	}
-	for _, open := range grid.OpenSquares(start) {
-		if _, ok := seen[*open]; ok {
+	return nodes
+}
+
+func (g *Graph) AttackInRangeTarget(node *Node) bool {
+	if node.Piece == nil {
+		log.Fatalf("node piece is empty: %+v", node)
+	}
+	targets := make([]*Node, 0)
+	for _, adj := range g.Adjacent(node) {
+		if adj.Piece == nil || node.Piece.Type == adj.Piece.Type {
 			continue
 		}
-		pathCopy := make(Path, len(path))
-		copy(pathCopy, path)
-		pathCopy = append(pathCopy, open)
-		if open.X == end.X && open.Y == end.Y {
-			paths = append(paths, pathCopy)
-		} else {
-			paths = grid.getPathsOne(open, end, pathCopy, paths)
-		}
-	}
-	return paths
-}
-
-func (grid Grid) TotalHitPoints() int {
-	total := 0
-	for _, row := range grid {
-		for _, piece := range row {
-			if piece.Type == TypeElf || piece.Type == TypeGoblin {
-				total += piece.HitPoints
-			}
-		}
-	}
-	return total
-}
-
-func (grid Grid) Move(source, dest *Piece) {
-
-	if source != grid[source.Y][source.X] {
-		log.Fatalf("source is not the same piece on the grid: source=%+v, grid=%+v\n", source, grid[source.Y][source.X])
-	}
-	if !(source.Type == TypeElf || source.Type == TypeGoblin) {
-		log.Fatalf("source is not of type elf or goblin: %+v\n", source)
-	}
-
-	if dest != grid[dest.Y][dest.X] {
-		log.Fatalf("dest is not the same piece on the grid: dest=%+v, grid=%+v\n", dest, grid[dest.Y][dest.X])
-	}
-	if dest.Type != TypeOpen {
-		log.Fatalf("destination square is not open: %+v\n", dest)
-	}
-
-	// swap source and dest positions
-	source.X, dest.X = dest.X, source.X
-	source.Y, dest.Y = dest.Y, source.Y
-
-	grid[source.Y][source.X] = source
-	grid[dest.Y][dest.X] = dest
-
-}
-
-func (grid Grid) OpenSquares(unit *Piece) []*Piece {
-	squares := make([]*Piece, 0)
-	for _, square := range grid.Adjacent(unit) {
-		if square.Type == TypeOpen {
-			squares = append(squares, square)
-		}
-	}
-	return squares
-}
-
-func (grid Grid) AttackInRangeTarget(unit *Piece) bool {
-	targets := make([]*Piece, 0)
-	for _, adjacent := range grid.Adjacent(unit) {
-		if unit.IsEnemy(adjacent) {
-			targets = append(targets, adjacent)
-		}
+		targets = append(targets, adj)
 	}
 
 	if len(targets) == 0 {
@@ -237,81 +226,27 @@ func (grid Grid) AttackInRangeTarget(unit *Piece) bool {
 	target := targets[0]
 
 	// attack!!!
-	target.HitPoints -= 3
+	target.Piece.HitPoints -= 3
 
-	// if the target has zero or fewer hit points, it dies (replace it with an open square)
-	if target.HitPoints <= 0 {
-		target.Type = TypeOpen
+	// if the target piece has zero or fewer hit points, it dies
+	if target.Piece.HitPoints <= 0 {
+		target.Piece = nil
 	}
 
 	return true
 }
 
-func (grid Grid) Adjacent(unit *Piece) []*Piece {
-	pieces := make([]*Piece, 0)
-
-	adjacent := []*Piece{
-		&Piece{X: unit.X, Y: unit.Y - 1}, // up
-		&Piece{X: unit.X, Y: unit.Y + 1}, // down
-		&Piece{X: unit.X - 1, Y: unit.Y}, // left
-		&Piece{X: unit.X + 1, Y: unit.Y}, // right
-	}
-
-	for _, adj := range adjacent {
-		if adj.Y < 0 || adj.Y > len(grid) {
-			continue
-		}
-		if adj.X < 0 || adj.X > len(grid[adj.Y]) {
-			continue
-		}
-		piece := grid[adj.Y][adj.X]
-		if piece.Type != TypeWall {
-			pieces = append(pieces, piece)
+func (g *Graph) TotalHitPoints() int {
+	total := 0
+	for _, node := range g.Nodes {
+		if node.Piece != nil {
+			total += node.Piece.HitPoints
 		}
 	}
-	return pieces
+	return total
 }
 
-func (grid Grid) Targets(unit *Piece) []*Piece {
-	targets := make([]*Piece, 0)
-	for _, row := range grid {
-		for _, piece := range row {
-			if unit.IsEnemy(piece) {
-				targets = append(targets, piece)
-			}
-		}
-	}
-	return targets
-}
-
-func (piece *Piece) IsEnemy(target *Piece) bool {
-	return (piece.Type == TypeElf && target.Type == TypeGoblin) || (piece.Type == TypeGoblin && target.Type == TypeElf)
-}
-
-type ByHitPoints []*Piece
-
-func (s ByHitPoints) Len() int {
-	return len(s)
-}
-func (s ByHitPoints) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s ByHitPoints) Less(i, j int) bool {
-
-	// primary sort: fewest hit points
-	if s[i].HitPoints != s[j].HitPoints {
-		return s[i].HitPoints < s[j].HitPoints
-	}
-
-	// secondary sort: reading order
-	if s[i].Y != s[j].Y {
-		return s[i].Y < s[j].Y
-	}
-	return s[i].X < s[j].X
-}
-
-type ByShortestPath Paths
+type ByShortestPath []Path
 
 func (s ByShortestPath) Len() int {
 	return len(s)
@@ -337,9 +272,9 @@ func (s ByShortestPath) Less(i, j int) bool {
 		return lastPieceI.X < lastPieceJ.X
 	}
 
-	// tertiary sort: reading order of first piece
-	firstPieceI := s[i][0]
-	firstPieceJ := s[j][0]
+	// tertiary sort: reading order of first adjacent piece
+	firstPieceI := s[i][1]
+	firstPieceJ := s[j][1]
 	if firstPieceI.Y != firstPieceJ.Y {
 		return firstPieceI.Y < firstPieceJ.Y
 	}
@@ -347,40 +282,119 @@ func (s ByShortestPath) Less(i, j int) bool {
 
 }
 
-func (g Grid) Print() {
-	for _, row := range g {
-		hits := make([]string, 0)
-		for _, piece := range row {
-			fmt.Print(string(piece.Type))
-			if piece.Type == TypeElf || piece.Type == TypeGoblin {
-				hits = append(hits, fmt.Sprintf("%s(%d)", string(piece.Type), piece.HitPoints))
-			}
-		}
-		fmt.Println("   " + strings.Join(hits, ", "))
-	}
-	fmt.Println()
+type ByPosition []*Node
+
+func (s ByPosition) Len() int {
+	return len(s)
+}
+func (s ByPosition) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
 }
 
-func NewGrid(lines []string) Grid {
-	grid := make(Grid, len(lines))
+func (s ByPosition) Less(i, j int) bool {
 
-	for y, line := range lines {
-		row := make([]*Piece, len(line))
-		for x, char := range line {
-			typ := Type(char)
-			switch typ {
-			case TypeElf, TypeGoblin:
-				row[x] = &Piece{x, y, typ, 200}
-			case TypeWall, TypeOpen:
-				row[x] = &Piece{x, y, typ, 0}
-			default:
-				log.Fatalf("unexpected char in grid data at %d,%d: [%s]\n", x, y, string(char))
-			}
-		}
-		grid[y] = row
+	// sort by reading order
+	if s[i].Y != s[j].Y {
+		return s[i].Y < s[j].Y
+	}
+	return s[i].X < s[j].X
+}
+
+type ByHitPoints []*Node
+
+func (s ByHitPoints) Len() int {
+	return len(s)
+}
+func (s ByHitPoints) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s ByHitPoints) Less(i, j int) bool {
+
+	// primary sort: fewest hit points
+	if s[i].Piece.HitPoints != s[j].Piece.HitPoints {
+		return s[i].Piece.HitPoints < s[j].Piece.HitPoints
 	}
 
-	return grid
+	// secondary sort: reading order
+	if s[i].Y != s[j].Y {
+		return s[i].Y < s[j].Y
+	}
+	return s[i].X < s[j].X
+}
+
+func NewGraph(lines []string) *Graph {
+
+	grid := make(map[int]map[int]*Node)
+	nodes := make([]*Node, 0)
+
+	id := -1
+	for y, line := range lines {
+		for x, char := range line {
+			if char == TypeWall {
+				continue
+			}
+
+			id++
+			node := &Node{
+				ID: id,
+				X:  x,
+				Y:  y,
+			}
+
+			if _, ok := grid[x]; !ok {
+				grid[x] = make(map[int]*Node)
+			}
+
+			grid[x][y] = node
+			nodes = append(nodes, node)
+
+			if char == TypeOpen {
+				continue
+			}
+			piece := &Piece{
+				Type:      Type(char),
+				HitPoints: 200,
+			}
+			node.Piece = piece
+		}
+	}
+
+	// store the edges in an adjacency matrix
+	edges := make([][]bool, len(nodes))
+	for i := 0; i < len(edges); i++ {
+		edges[i] = make([]bool, len(nodes))
+	}
+
+	for i, node := range nodes {
+		if i != node.ID {
+			log.Fatalf("node index=%d not equal to id=%d\n", i, node.ID)
+		}
+		if _, ok := grid[node.X][node.Y]; ok {
+			//fmt.Printf("node exists: %+v\n", node)
+
+			neighbors := []struct {
+				x, y int
+			}{
+				{node.X, node.Y - 1},
+				{node.X - 1, node.Y},
+				{node.X + 1, node.Y},
+				{node.X, node.Y + 1},
+			}
+			for _, n := range neighbors {
+				if adj, ok := grid[n.x][n.y]; ok {
+					edges[node.ID][adj.ID] = true
+					edges[adj.ID][node.ID] = true
+				}
+			}
+		}
+	}
+
+	return &Graph{
+		Nodes: nodes,
+		Edges: edges,
+	}
+
 }
 
 func readFile(path string) ([]string, error) {
